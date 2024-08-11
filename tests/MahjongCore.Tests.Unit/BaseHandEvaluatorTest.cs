c234a0b9a790479987726a931b184361e7735a07
DanyloNikulin <nikulindaniil1997@gmail.com>
2024-08-11 11:55:47 +0200

    Merge branch 'master' into implement_HandDivider

---
+++ tests/MahjongCore.Tests.Unit/BaseHandEvaluatorTest.cs
@@ -0,0 +1,28 @@
+﻿namespace MahjongCore.Tests.Unit;
+
+using Application;
+using Domain.Models;
+using FluentAssertions;
+using TestSets.Common;
+using Xunit;
+
+public class BaseHandEvaluatorTest
+{
+    private readonly BaseHandEvaluator _sut;
+
+    public BaseHandEvaluatorTest()
+    {
+        _sut = new BaseHandEvaluator();
+    }
+
+    [Theory]
+    [ClassData(typeof(BaseHandEvaluatorWhenItExists))]
+    public void Run_ReturnsExpectedYaku_WhenItExists(GameState state, List<Yaku> expected)
+    {
+        // Act
+        var result = _sut.Run(state);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ tests/MahjongCore.Tests.Unit/TestSets/Common/BaseTestSet.cs
@@ -0,0 +1,18 @@
+﻿namespace MahjongCore.Tests.Unit.TestSets.Common;
+
+using System.Collections;
+
+public  abstract class BaseTestSet : IEnumerable<object[]>
+{
+    protected readonly List<object?[]> _data = new();
+    
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        return _data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
